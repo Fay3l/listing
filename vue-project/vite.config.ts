2225dd0209af7b3b7378e0bd4718479bd3c73d8f
Fay3l <mohamed.fayel@yahoo.com>
2026-06-11 17:13:25 +0200

    remove package vite plugiin html

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -4,21 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    createHtmlPlugin({
-      inject:{
-        data:{
-          title: 'MyLyst - Organize your lists easily',
-          description: 'MyLyst is a web application that allows you to create and manage lists of items. It is designed to be simple and easy to use, while also providing powerful features to help you stay organized.',
-          ogImage: 'https://mylyst.app/og-image.jpg',
-        }
-      }
-    }),
     tailwindcss(),
     vueDevTools(),
   ],
