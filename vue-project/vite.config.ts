568aaf9c1ab181a13f052eb8bc2bc2cbd4770325
Fay3l <mohamed.fayel@yahoo.com>
2026-06-11 16:53:10 +0200

    add meta for google search add og image and robots.txt

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -4,11 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createHtmlPlugin({
+      inject:{
+        data:{
+          title: 'MyMyst - Organize your lists easily',
+          description: 'MyMyst is a web application that allows you to create and manage lists of items. It is designed to be simple and easy to use, while also providing powerful features to help you stay organized.',
+          ogImage: 'https://mymyst.app/og-image.jpg',
+        }
+      }
+    }),
     tailwindcss(),
     vueDevTools(),
   ],
