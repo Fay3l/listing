f6787dff0020eef92976bc527536a0bc2c7095fe
Fay3l <mohamed.fayel@yahoo.com>
2026-06-11 17:03:30 +0200

    change title description

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
     createHtmlPlugin({
       inject:{
         data:{
-          title: 'MyMyst - Organize your lists easily',
-          description: 'MyMyst is a web application that allows you to create and manage lists of items. It is designed to be simple and easy to use, while also providing powerful features to help you stay organized.',
-          ogImage: 'https://mymyst.app/og-image.jpg',
+          title: 'MyLyst - Organize your lists easily',
+          description: 'MyLyst is a web application that allows you to create and manage lists of items. It is designed to be simple and easy to use, while also providing powerful features to help you stay organized.',
+          ogImage: 'https://mylyst.app/og-image.jpg',
         }
       }
     }),
